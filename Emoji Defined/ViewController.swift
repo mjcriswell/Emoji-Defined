9769c1b04a4ad228a30784b58fa23bca20f7ea86
mjcriswell <mjcriswell@michaels-mbp.gateway.2wire.net>
2014-10-30 13:04:45 -0700

    Initial Commit

---
+++ Emoji Defined/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Defined
+//
+//  Created by michael criswell on 10/30/14.
+//  Copyright (c) 2014 michael criswell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
